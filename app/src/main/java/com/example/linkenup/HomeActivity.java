888625c36cd44617c8d5126e0f3bf34e83309056
= <=>
2020-11-09 18:19:35 -0300

    HomeLayout Preferences

--- app/src/main/java/com/example/linkenup/HomeActivity.java
+++ app/src/main/java/com/example/linkenup/HomeActivity.java
@@ -4,13 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 import android.view.View;
+import android.widget.GridLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+    public static final String LAYOUT_PREFERENCE_NAME = "com.example.linkenup.HomeActivity.layout_preference";
+
 
     Toolbar toolbar;
+    GridLayout gridLayout;
+    RelativeLayout mainLayout;
+    ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +29,34 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         toolbar = (Toolbar) findViewById(R.id.home_toolbar);
+        gridLayout = (GridLayout) findViewById(R.id.home_layout_smallimg);
+        mainLayout = (RelativeLayout) findViewById(R.id.home_relative_main);
+        scrollView = (ScrollView) findViewById(R.id.home_scroll_main);
 
+        SharedPreferences settings = getSharedPreferences(LAYOUT_PREFERENCE_NAME,0);
+        boolean smallimg = settings.getBoolean("smallimg",false);
+        gridLayout.setVisibility(smallimg?View.VISIBLE:View.GONE);
+
+        if(smallimg) scrollView.setVisibility(View.GONE); else {
+            boolean img, btn;
+            img = settings.getBoolean("img",true);
+            btn = settings.getBoolean("btn",true);
+            int imgVisibility = img ? View.VISIBLE : View.GONE;
+            int btnVisibility = btn ? View.VISIBLE : View.GONE;
+            int dividerVisibility = img && btn ? View.VISIBLE : View.GONE;
+
+            int i = 1;
+            while (i != -1) {
+                try {
+                    mainLayout.getChildAt(i * 3 - 3).setVisibility(imgVisibility);//imgs
+                    mainLayout.getChildAt(i * 3 - 2).setVisibility(btnVisibility);//btns
+                    mainLayout.getChildAt(i * 3 - 1).setVisibility(dividerVisibility);//dividers
+                    i++;
+                } catch (Exception ex) {
+                    i = -1;
+                }
+            }
+        }
         String newTitle = "<"+toolbar.getTitle()+">";
         toolbar.setTitle(newTitle);
     }
--- app/src/main/java/com/example/linkenup/PreferenceActivity.java
+++ app/src/main/java/com/example/linkenup/PreferenceActivity.java
@@ -2,18 +2,96 @@ package com.example.linkenup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
 
 public class PreferenceActivity extends AppCompatActivity {
+    boolean home_imgVisible,home_btnVisible,home_smallimg;
+
+    CheckBox chkHomeLayout_img,chkHomeLayout_btn,chkHomeLayout_smallimg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference);
+        chkHomeLayout_img = (CheckBox) findViewById(R.id.preference_check_homelayout_img);
+        chkHomeLayout_btn = (CheckBox) findViewById(R.id.preference_check_homelayout_btn);
+        chkHomeLayout_smallimg = (CheckBox) findViewById(R.id.preference_check_homelayout_smallimg);
+
+        SharedPreferences homeLayout = getSharedPreferences(HomeActivity.LAYOUT_PREFERENCE_NAME,0);
+
+        boolean img, btn, smallimg;
+        img = homeLayout.getBoolean("img",true);
+        btn = homeLayout.getBoolean("btn",true);
+        smallimg =homeLayout.getBoolean("smallimg",false);
+
+        chkHomeLayout_img.setChecked(img);
+        chkHomeLayout_btn.setChecked(btn);
+        chkHomeLayout_smallimg.setChecked(smallimg);
+        home_imgVisible = img;
+        home_btnVisible = btn;
+        home_smallimg = smallimg;
+
+    }
+
+    public void updatePreferences(String preference)
+    {
+    switch (preference)
+    {
+        case "homelayout":
+            SharedPreferences.Editor setting = getSharedPreferences(HomeActivity.LAYOUT_PREFERENCE_NAME,0).edit();
+            setting.putBoolean("img",home_imgVisible);
+            setting.putBoolean("btn",home_btnVisible);
+            setting.putBoolean("smallimg",home_smallimg);
+            setting.commit();
+            break;
+    }
     }
 
     public void onHomeLayout(View view){
+        int id = view.getId();
+        if(id == R.id.preference_check_homelayout_img) {
+            if (home_imgVisible = ((CheckBox) view).isChecked())
+                home_smallimg = false;
+            else if (!home_btnVisible)
+                home_smallimg = true;
+        }
 
+        if(id == R.id.preference_check_homelayout_btn) {
+            if (home_btnVisible = ((CheckBox) view).isChecked()) {
+                home_smallimg = false;
+            } else if (!home_imgVisible) {
+                home_smallimg = true;
+            }
+        }
+
+        if(id == R.id.preference_check_homelayout_smallimg){
+            if(home_smallimg = ((CheckBox) view).isChecked())
+            {
+                home_imgVisible = false;
+                home_btnVisible = false;
+            }else
+            if(!home_btnVisible && !home_imgVisible)
+            {
+                home_imgVisible = true;
+                home_btnVisible = true;
+            }
+        }
+
+        chkHomeLayout_img.setChecked(home_imgVisible);
+        chkHomeLayout_btn.setChecked(home_btnVisible);
+        chkHomeLayout_smallimg.setChecked(home_smallimg);
+
+
+        updatePreferences("homelayout");
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(this,HomeActivity.class));
     }
 }
